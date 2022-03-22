5178c05c21fd5aebc5ea550fdde61e0301d27434
Harisfu123 <100756594+Harisfu123@users.noreply.github.com>
2022-03-22 15:16:55 +0700

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,14 @@
+// Masukan suatu angka
+// Menukar variabel a dan b dengan operator matematika
+
+let a = prompt('Masukan variabel pertama: ');
+let b = prompt('Masukan variabel kedua: ');
+
+// TODO: answer here
+let temp;
+temp = a;
+b = a;
+a = b;
+
+console.log(`Nilai a setelah ditukar: ${a}`);
+console.log(`Nilai b setelah ditukar: ${b}`);
