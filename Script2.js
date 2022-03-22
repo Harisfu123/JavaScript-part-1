d992ca983be1d348bf0513c69ab9d58b320878a6
Harisfu123 <100756594+Harisfu123@users.noreply.github.com>
2022-03-22 14:51:07 +0700

    Create Script2.js

---
+++ Script2.js
@@ -0,0 +1,10 @@
+const num1 = parseInt(prompt("Masukan bilangan pertama: "));
+const num2 = parseInt(prompt("Masukan bilangan kedua: "));
+const num3 = parseInt(prompt("Masukan bilangan ketiga: "));
+
+let largest;
+
+// TODO: answer here
+largest = Math.max(num1, num2, num3);
+// Menampilkan hasil
+console.log("Bilangan terbesar adalah " + largest);
